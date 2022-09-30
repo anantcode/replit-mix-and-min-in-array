813a1e3452fe833e5ffe0db16053d1d499abdd88
anantcode <36257780+anantcode@users.noreply.github.com>
2022-09-30 04:42:24 +0000

    update

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
 
+// ye to bahut basic hai.
+// only that recursive approch is new. wo bhi tree nahi banata hai. other thing is to make it easier could have used left right pointer and while.
+
 /*
 
-Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
-
-Input: arr[] = {3, 5, 4, 1, 9}
-Output: Minimum element is: 1
-              Maximum element is: 9
-
-Input: arr[] = {22, 14, 8, 17, 35, 3}
-Output:  Minimum element is: 3
-              Maximum element is: 35
+PROBLEM:
+  
+  Given an array of size N. The task is to find the maximum and the minimum element of the array using the minimum number of comparisons.
+  
+  Input: arr[] = {3, 5, 4, 1, 9}
+  Output: Minimum element is: 1
+                Maximum element is: 9
+  
+  Input: arr[] = {22, 14, 8, 17, 35, 3}
+  Output:  Minimum element is: 3
+                Maximum element is: 35
 */
 
 
